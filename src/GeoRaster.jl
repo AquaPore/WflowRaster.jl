04490e63709b244c637b7be674de5b922f940cba
AquaPore <pollacco.water@gmail.com>
2025-04-02 09:37:41 +0100

    Update

--- src/GeoRaster.jl
+++ src/GeoRaster.jl
@@ -25,49 +25,60 @@ module geoRaster
     """
         Deriving metadata of the GeoTiff file
     """
-    function RASTER_METADATA(Path; Verbose=true)
 
-        Grid = Rasters.Raster(Path, lazy=true)
-            N_Width = size(Grid, X)
-            N_Height = size(Grid, Y)
-            ΔX =  step(dims(Grid, X)) |> abs
-            ΔY =  step(dims(Grid, Y)) |> abs
-            Coord_X_Left   = first(dims(Grid, X))
-            Coord_X_Right  = last(dims(Grid, X))
-            Coord_Y_Top    = first(dims(Grid ,Y))
-            Coord_Y_Bottom = last(dims(Grid,Y))
 
-        Grid_GeoTIFF = GeoTIFF.load(Path)
-            Grid_GeoTIFF_Metadata = GeoTIFF.metadata(Grid_GeoTIFF)
-                Crs =GeoTIFF.epsgcode(Grid_GeoTIFF_Metadata) |>Int
+    # ================================================================
+    #		FUNCTION : RASTER_METADATA
+    # ================================================================
+        function RASTER_METADATA(Path; Verbose=true)
 
-		Grid_Ag = AG.readraster(Path)
-            Bands = AG.nraster(Grid_Ag)
+            Grid = Rasters.Raster(Path, lazy=true)
+                N_Width = size(Grid, X)
+                N_Height = size(Grid, Y)
+                ΔX =  step(dims(Grid, X)) |> abs
+                ΔY =  step(dims(Grid, Y)) |> abs
+                Coord_X_Left   = first(dims(Grid, X))
+                Coord_X_Right  = last(dims(Grid, X))
+                Coord_Y_Top    = first(dims(Grid ,Y))
+                Coord_Y_Bottom = last(dims(Grid,Y))
 
-        if Verbose
-            println(Path)
-            println("Bands = $Bands")
-            println("Crs = $Crs")
-            println("ΔX = $ΔX")
-            println("ΔY = $ΔY")
-            println("N_Width  = $N_Width") 
-            println("N_Height = $N_Height")
-            println("Coord_X_Left = $Coord_X_Left, Coord_X_Right = $Coord_X_Right")
-            println("Coord_Y_Top = $Coord_Y_Top, Coord_Y_Bottom = $Coord_Y_Bottom")
+            Grid_GeoTIFF = GeoTIFF.load(Path)
+                Grid_GeoTIFF_Metadata = GeoTIFF.metadata(Grid_GeoTIFF)
+                    Crs =GeoTIFF.epsgcode(Grid_GeoTIFF_Metadata) |>Int
+
+            Grid_Ag = AG.readraster(Path)
+                Bands = AG.nraster(Grid_Ag)
+
+            if Verbose
+                println(Path)
+                println("Bands = $Bands")
+                println("Crs = $Crs")
+                println("ΔX = $ΔX")
+                println("ΔY = $ΔY")
+                println("N_Width  = $N_Width") 
+                println("N_Height = $N_Height")
+                println("Coord_X_Left = $Coord_X_Left, Coord_X_Right = $Coord_X_Right")
+                println("Coord_Y_Top = $Coord_Y_Top, Coord_Y_Bottom = $Coord_Y_Bottom")
+            end
+
+            Metadata = METADATA(N_Width, N_Height, ΔX, ΔY, Coord_X_Left, Coord_X_Right,Coord_Y_Top, Coord_Y_Bottom, Crs, Bands)
+
+        return Metadata
+        end # function RASTER_METADATA 
+    # ----------------------------------------------------------------
+
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #		FUNCTION : iXY_2_COORD
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        using GeoArrays
+        function iXY_2_COORD(iX, iY, Path)
+            Grid = GeoArrays.read(Path)
+
+            Coord_X, Coord_Y = GeoArrays.coords(Grid, (iX, iY))
+
+            println(Coord_X," " ,Coord_Y)
         end
-
-        Metadata = METADATA(N_Width, N_Height, ΔX, ΔY, Coord_X_Left, Coord_X_Right,Coord_Y_Top, Coord_Y_Bottom, Crs, Bands)
-
-    return Metadata
-    end
-
-    using GeoArrays
-    function iX_2_COORD(iX, iY, Path)
-        Grid = GeoArrays.read(Path)
-
-        Coord_X, Coord_Y = GeoArrays.coords(Grid, (iX, iY))
-
-        println(Coord_X," " ,Coord_Y)
-    end
+    # ----------------------------------------------------------------
 
 end #module geoRaster
